2685eab98d28e7aa51611f38c78524d3761dd3a7
Joao Marcelo <jjmarcelo2@gmail.com>
2020-08-25 00:38:54 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá, <b>Sharp Solucões Digitais :)</b>
-        </p>
-        <a
-          className="App-link"
-          href="https://sharpsolucoes.com"
-          target="_blank"
-        >
-          Nosso site
-        </a>
-      </header>
-    </div>
+    <Routes />
+    
   );
 }
-
 export default App;
